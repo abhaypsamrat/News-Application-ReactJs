6737a60605bd8ba1a9453ffe65804be47f4094c6
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-01-29 18:18:16 +0530

    design news item structure

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
     return (
-      <div>this is News components
-        <NewsItem />
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
+
     return (
-      <div>NewsItem</div>
-    )
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://www.reuters.com/resizer/WoQD7LHyzH_QKIwIdO7BUY3O0SM=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/UXVMS35M7VJHBN7HHNPZV3EBK4.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
