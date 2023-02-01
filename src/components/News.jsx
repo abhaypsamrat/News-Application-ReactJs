3050dbdad5bb6209480644a30c97b1d4ea50dbfe
abhaypsamrat <abhay.p.samrat@gmail.com>
2023-02-01 18:32:03 +0530

    adding next & previous button for more news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,17 +7,58 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=0bbb3e8a886d4286b6f19eac177132c1";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=0bbb3e8a886d4286b6f19eac177132c1&page=1pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
     //console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+
+  //   handlePrevClick is used for previous news page
+
+  handlePrevClick = async () => {
+    console.log("previous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=0bbb3e8a886d4286b6f19eac177132c1&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    //console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  //   handleNextClick is used for Next news page
+
+  handleNextClick = async () => {
+    console.log("Next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=0bbb3e8a886d4286b6f19eac177132c1&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      //console.log(parsedData);
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -38,6 +79,23 @@ export default class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-around">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-info"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
